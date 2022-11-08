948bba47768eeb058515d1e5f766128ce7617c20
Nimish07022002 <nimishsuri5@gmail.com>
2022-11-08 15:30:24 +0530

    code added

--- src/home.js
+++ src/home.js
@@ -1,34 +1,50 @@
-let speed = 740 // speed of plane in km/h
+let planes = []
 
-let planes = [
-    { name: 'plane-1', distance: 500, fuel: 1500 },
-    { name: 'plane-2', distance: 400, fuel: 1800 },
-    { name: 'plane-3', distance: 390, fuel: 1100 },
-    { name: 'plane-4', distance: 100, fuel: 1200 },
-    { name: 'plane-5', distance: 700, fuel: 1600 },
-    { name: 'plane-6', distance: 450, fuel: 1200 }
-]
 
-let i = 0
 let ps = document.querySelectorAll('.plane p')
-ps.forEach(e => { Array.from(e.innerText = "Name : " + planes[i]['name'] + ", Distance : " + planes[i]['distance'] + ", Fuel : " + planes[i++]['fuel']) })
+ps.forEach((e,i) => { Array.from(e.innerText = "Name : " + planes[i]['name'] + ", Distance : " + planes[i]['distance'] + ", Fuel : " + planes[i++]['fuel']) })
+
+const CreatePlane=(param)=>{
+    return {'name':param[0],'fuel':param[1],'type':param[2],'emergency':param[3],'priority':undefined}
+}
+
+const Createrunway=()=>{
+    return {}
+}
+
+const Setpriority=()=>{
+    
+}
+
+function submit(){
+    n=document.getElementById('name').value
+    f=document.getElementById('fuel').value
+    t=document.getElementById('type').value
+    e=document.getElementById('emergency').checked
+
+    planes.push(CreatePlane([n,f,t,e]))
+}
 
 function run() {
     const element = document.getElementsByClassName("plane");
-    setInterval(frame, 0.51);
+    const interval = setInterval(frame, 0.5);
+    let i = 0
     let width = 0;
     function frame() {
-        // if (i < 6) {
+        console.log(`${i} plane is changing`)
+        if (i < 6) {
             if (width > 685) {
                 i++
                 width = 0
             }
             else {
-                width++;
+                width += 5;
                 element[i % 6].style.left = (width / 10) + '%';
             }
-        // }
-        // else
-        //     clearInterval(frame)
+        }
+        else {
+            clearInterval(interval)
+            console.log('Width function stoped')
+        }
     }
 }
