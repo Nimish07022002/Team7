f20895983fd6011811b8fc236531fbb36c6ee23c
Nimish07022002 <nimishsuri5@gmail.com>
2022-11-10 22:24:20 +0530

    functions updated

---
+++ src/src/components/manual.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import { Simulate } from './simulate'
+import { useState } from 'react'
+import { useEffect } from 'react'
+
+export const Manual = () => {
+  const [name, setName] = useState("")
+  const [fuel, setFuel] = useState(0)
+  const [type, setType] = useState("")
+  const [emergency, setEmergency] = useState(false)
+  const [planes, addPlanes] = useState(()=>[])
+  
+
+  function run() {
+    const element = document.getElementsByClassName("plane");
+    const interval = setInterval(frame, 0.5);
+    let i = 0
+    let width = 0;
+    function frame() {
+      console.log(`${i} plane is changing`)
+      if (i < 4) {
+        if (width > 685) {
+          i++
+          width = 0
+        }
+        else {
+          width += 5;
+          element[i % 4].style.left = (width / 10) + '%';
+        }
+      }
+      else {
+        clearInterval(interval)
+        console.log('Width function stoped')
+      }
+    }
+  }
+
+  useEffect(()=>{
+    console.log(planes)
+  },[planes])
+
+  const submit =(e)=>{
+    let nP={
+      name: name,
+      fuel: fuel,
+      type: type,
+      emergency: emergency,
+      priority: undefined,
+    };
+    addPlanes([...planes,nP])
+  }
+  return (
+    <div>
+      <Simulate/>
+      <div id="inp-form">
+            <label htmlFor="">Name : <input value={name} onChange={(e)=>setName(e.target.value)} id="name" type="text" required/></label><br/>
+            <label htmlFor="">Fuel : &nbsp;&nbsp;<input value={fuel} onChange={(e)=>setFuel(e.target.value)} id="fuel" type="number" required/></label><br/>
+            <label htmlFor="">Type :&nbsp;
+                <select onChange={(e)=>setType(e.target.value)} id="ptype" name="Ptype" required>
+                    <option value="select">Select</option>
+                    <option value="plane1">plane1</option>
+                    <option value="plane2">plane2</option>
+                    <option value="plane3">plane3</option>
+                </select>
+            </label>
+            <br/>
+            <label onChange={(e)=>setEmergency(e.target.value)} htmlFor="">Emergency :<br/>
+                <input name="emergency" type="radio" value="None"/>None&nbsp;&nbsp;
+                <input name="emergency" type="radio" value="Medical"/>Medical&nbsp;&nbsp;
+                <input name="emergency" type="radio" value="Technical"/>Technical
+            </label><br/>
+            <button id="Generate" onClick={submit}>Generate</button>
+            <button onClick={run} id="Simulate">Simulate</button>
+        </div>
+    </div>
+  )
+}
---
+++ src/src/components/simulate.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+
+export const Simulate = () => {
+
+  
+  return (
+    <div id="simulate">
+      <div id="left">
+        <h3>Holding Area</h3>
+        <div className='plane-area'>
+          <div className='plane'>
+            <p>Name : a123, Type: Cargo, Emergency: YES</p>
+          </div><div className='plane'>
+            <p>Name : a124, Type: Passenger, Emergency: NO</p>
+          </div><div className='plane'>
+            <p>Name : a125, Type: Jet, Emergency: NO</p>
+          </div><div className='plane'>
+            <p>Name : a124, Type: Passenger, Emergency: NO</p>
+          </div>
+        </div>
+      </div>
+      <div id="center">
+        <h3>Landing Area</h3>
+        <hr style={{ marginTop: '40%', color: 'black', fontWeight: '800' }} />
+      </div>
+      <div id="right">
+        <h3>Airport</h3>
+      </div>
+     <br />
+      <table id="plane-dt">
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Fuel left</th>
+            <th>Status</th>
+          </tr>
+          <tr>
+            <td className="landed">Plane-1</td>
+            <td>45%</td>
+            <td>landed</td>
+          </tr>
+          <tr>
+            <td className="crashed">Plane-2</td>
+            <td>15%</td>
+            <td>crashed</td>
+          </tr>
+          <tr>
+            <td className="landed">Plane-3</td>
+            <td>55%</td>
+            <td>landed</td>
+          </tr>
+          <tr>
+            <td className="landed">Plane-4</td>
+            <td>43.5%</td>
+            <td>landed</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
